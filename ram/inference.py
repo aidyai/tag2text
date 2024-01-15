236047d53653a1350052911c6f6d278fa0e27e67
aidyosu <aidyosu553@gmail.com>
2024-01-15 15:31:45 -0800

    Update inference.py

--- ram/inference.py
+++ ram/inference.py
@@ -30,17 +30,3 @@ def inference_tag2text(image, model, input_tag="None"):
         return tag_predict[0], input_tag[0], caption[0]
 
 
-def inference_ram(image, model):
-
-    with torch.no_grad():
-        tags, tags_chinese = model.generate_tag(image)
-
-    return tags[0],tags_chinese[0]
-
-
-def inference_ram_openset(image, model):
-
-    with torch.no_grad():
-        tags = model.generate_tag_openset(image)
-
-    return tags[0]
