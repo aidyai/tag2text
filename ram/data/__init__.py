7aece81315f4aa336f5ba6862175fc25b9646d6a
aidyosu <aidyosu553@gmail.com>
2024-09-28 05:37:29 +0100

    __init__.py

--- ram/data/__init__.py
+++ ram/data/__init__.py
@@ -6,35 +6,40 @@ from torchvision.transforms.functional import InterpolationMode
 from .dataset import finetune_dataset
 from .randaugment import RandomAugment
 
-def create_dataset(dataset, config, min_scale=0.5):
+def create_dataset(data_id, config, min_scale=0.5):
+    normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), 
+                                     (0.26862954, 0.26130258, 0.27577711))
+
+    # Define transformation for training
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(config['image_size'], scale=(min_scale, 1.0), interpolation=InterpolationMode.BICUBIC),
+        transforms.RandomHorizontalFlip(),
+        RandomAugment(2, 5, isPIL=True, augs=[
+            'Identity', 'AutoContrast', 'Brightness', 'Sharpness', 'Equalize', 
+            'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    # Define transformation for 224 input size
+    transform_inputsize_224 = transforms.Compose([
+        transforms.RandomResizedCrop(224, scale=(min_scale, 1.0), interpolation=InterpolationMode.BICUBIC),
+        transforms.RandomHorizontalFlip(),
+        RandomAugment(2, 5, isPIL=True, augs=[
+            'Identity', 'AutoContrast', 'Brightness', 'Sharpness', 'Equalize', 
+            'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    # Initialize the dataset using the finetune_dataset class
+    dataset = finetune_dataset(data_id=data_id, 
+                               transform=transform_train, 
+                               transform_224=transform_inputsize_224, 
+                               config=config)
     
-    normalize = transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    return dataset
 
-    transform_train = transforms.Compose([                        
-            transforms.RandomResizedCrop(config['image_size'],scale=(min_scale, 1.0),interpolation=InterpolationMode.BICUBIC),
-            transforms.RandomHorizontalFlip(),
-            RandomAugment(2,5,isPIL=True,augs=['Identity','AutoContrast','Brightness','Sharpness','Equalize',
-                                              'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
-            transforms.ToTensor(),
-            normalize,
-        ])        
-
-    transform_inputsize_224 = transforms.Compose([                        
-            transforms.RandomResizedCrop(224,scale=(min_scale, 1.0),interpolation=InterpolationMode.BICUBIC),
-            transforms.RandomHorizontalFlip(),
-            RandomAugment(2,5,isPIL=True,augs=['Identity','AutoContrast','Brightness','Sharpness','Equalize',
-                                              'ShearX', 'ShearY', 'TranslateX', 'TranslateY', 'Rotate']),     
-            transforms.ToTensor(),
-            normalize,
-        ])     
-
-    if dataset=='pretrain':
-        dataset = pretrain_dataset(config['train_file'], transform_train, class_num=config['class_num'], root=config['image_path_root'])              
-        return dataset  
-
-    elif dataset=='finetune':   
-        dataset = finetune_dataset(config['train_file'], transform_train, transform_inputsize_224, class_num=config['class_num'], root=config['image_path_root'])              
-        return dataset  
 
 
     
