a22cc12c1ba260ff2247a02e5aae2a572ed3a1ed
aidyosu <aidyosu553@gmail.com>
2024-09-28 05:27:04 +0100

    simple change to dataset

--- ram/data/dataset.py
+++ ram/data/dataset.py
@@ -17,50 +17,45 @@ import numpy as np
 
 
 class finetune_dataset(Dataset):
-    def __init__(self, ann_file, transform, transform_224, class_num = 4585, root = ''): 
-
-        self.ann = []
-        for f in ann_file:
-            print('loading '+f)
-            ann = json.load(open(f,'r'))
-            self.ann += ann
-            
+    def __init__(self, data_id, transform, transform_224, class_num=4585):
+        # Load the dataset from Hugging Face
+        self.dataset = load_dataset(data_id, split='train')
+        
         self.transform = transform
         self.transform_224 = transform_224
         self.class_num = class_num
-        self.root = root
 
-    
     def __len__(self):
-        return len(self.ann)
-    
-    def __getitem__(self, index):    
-        
-        ann = self.ann[index]   
+        return len(self.dataset)
 
-        image_path_use = os.path.join(self.root, ann['image_path'])
-        image = Image.open(image_path_use).convert('RGB')   
+    def __getitem__(self, index):
+        # Load the image, caption, and labels from the dataset
+        item = self.dataset[index]
+        
+        # The image is in PIL format directly from the dataset
+        image = item['image_path'].convert('RGB')
         image = self.transform(image)
 
-        image_224 = Image.open(image_path_use).convert('RGB')  
+        image_224 = item['image_path'].convert('RGB')
         image_224 = self.transform_224(image_224)
 
-        # required for tag2text support
-        if ann.get('union_label_id') is not None:
-            num = ann['union_label_id'] 
-            image_tag = np.zeros([self.class_num]) 
-            image_tag[num] = 1 
-            image_tag = torch.tensor(image_tag, dtype = torch.long)
+        # Process union_label_id (if available)
+        if item.get('union_label_id') is not None:
+            num = item['union_label_id']
+            image_tag = np.zeros([self.class_num])
+            image_tag[num] = 1
+            image_tag = torch.tensor(image_tag, dtype=torch.long)
         else:
             image_tag = None
 
-        caption_index = np.random.randint(0, len(ann['caption']))
+        # Select a random caption (if multiple captions exist)
+        caption_index = np.random.randint(0, len(item['caption']))
+        caption = pre_caption(item['caption'][caption_index], 30)
 
-        caption = pre_caption(ann['caption'][caption_index],30)
-
-        num = ann['parse_label_id'][caption_index]
+        # Process parse_label_id for the caption
+        num = item['parse_label_id'][caption_index]
         parse_tag = np.zeros([self.class_num])
         parse_tag[num] = 1
-        parse_tag = torch.tensor(parse_tag, dtype = torch.long)
+        parse_tag = torch.tensor(parse_tag, dtype=torch.long)
 
         return image, image_224, caption, image_tag, parse_tag
